f10bc13a6d1f06381c081cddc0c8d24f08665927
olshum <olshum8@gmail.com>
2024-05-22 23:41:47 +0300

    task solution with object destructuring and code refactoring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,17 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    {person.age === undefined ? (
-      ' '
-    ) : (
-      <p className="Person__age">I am {person.age}</p>
-    )}
-
-    {person.isMarried === false ? (
-      <p className="Person__partner">I am not married</p>
-    ) : (
-      <p className="Person__partner">
-        {person.sex === 'm'
-          ? `${person.partnerName} is my wife`
-          : `${person.partnerName} is my husband`}
-      </p>
-    )}
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {isMarried && partnerName ? (
+        <p className="Person__partner">
+          {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
